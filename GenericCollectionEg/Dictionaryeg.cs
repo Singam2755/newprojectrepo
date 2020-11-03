32e195f23abe3998947fb9ebdda931dda6e6e4dd
Singam2755 <singamganesh999@gmail.com>
2020-11-03 12:18:11 +0530

    adding one project

---
+++ GenericCollectionEg/Dictionaryeg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionEg
+{
+    class DictionaryEg
+    {
+        static void Main()
+        {
+            Dictionary<int, string> dt = new Dictionary<int, string>();
+
+            dt.Add(8, "ab");
+            dt.Add(9, "cd");
+            dt.Add(10, "bc");
+            dt.ContainsKey(9);
+            Console.WriteLine("Contains:{0}", dt.ContainsKey(9));
+            Console.WriteLine("Count:{0}", dt.Count());
+            foreach (KeyValuePair<int, string> k in dt)
+            {
+                Console.WriteLine(k.Key + " " + k.Value);
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ GenericCollectionEg/Queueeg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionEg
+{
+    class Queueeg
+    {
+        static void Main()
+
+        { 
+        Queue<string> q1 = new Queue<string>();
+            q1.Enqueue("singam");
+            q1.Enqueue("ganesh");
+            q1.Enqueue("singampalli");
+            foreach (string item in q1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
